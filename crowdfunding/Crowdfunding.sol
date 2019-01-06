2a35ea56375bc37a2297b693e97c6e703561b4e2
Danil Kharitsky <Khlive.ftf@gmail.com>
2019-01-07 00:11:56 +0500

    dapp repaired

--- crowdfunding/Crowdfunding.sol
+++ crowdfunding/Crowdfunding.sol
@@ -18,31 +18,22 @@ contract Crowdsale {
     event GoalReached(address recipient, uint totalAmountRaised);
     event FundTransfer(address backer, uint amount, bool isContribution);
 
-    /**
-     * Constructor function
-     *
-     * Setup the owner
-     */
+
     constructor(
         address ifSuccessfulSendTo,
         uint fundingGoalInEthers,
         uint durationInMinutes,
         uint etherCostOfEachToken,
         address addressOfTokenUsedAsReward
-    ) public {
+    ) public  {
         beneficiary = ifSuccessfulSendTo;
         fundingGoal = fundingGoalInEthers * 1 ether;
-        deadline = now + durationInMinutes * 1 minutes;
+        deadline = block.timestamp  +  durationInMinutes * 1 minutes;
         price = etherCostOfEachToken * 1 ether;
         tokenReward = token(addressOfTokenUsedAsReward);
     }
-
-    /**
-     * Fallback function
-     *
-     * The function without name is the default function that is called whenever anyone sends funds to a contract
-     */
-    function () payable public {
+    
+    function donate() payable public {
         require(!crowdsaleClosed);
         uint amount = msg.value;
         balanceOf[msg.sender] += amount;
@@ -53,48 +44,28 @@ contract Crowdsale {
 
     modifier afterDeadline() {if(now >= deadline) _;}
 
-    /**
-     * Check if goal was reached
-     *
-     * Checks if the goal or time limit has been reached and ends the campaign
-     */
     function checkGoalReached() public afterDeadline {
         if (amountRaised >= fundingGoal){
             fundingGoalReached = true;
             emit GoalReached(beneficiary, amountRaised);
         }
-        crowdsaleClosed = true;
+        crowdsaleClosed  = true;
     }
 	
-    /**
-     * Returns remained time of the campaign in minutes.
-     */
     function getTime() public view returns(uint){
-        uint time = (now - deadline) * 1 minutes;
+        uint time = (deadline - now);
         return time;
     }
 
-    /**
-     * Returns funding goal. 
-     */
     function getGoal() public view returns(uint){
         return fundingGoal;
     }
 
-    /**
-     * Returns raised funds.
-     */
+
     function getRaised() public view returns(uint){
         return amountRaised;
     }
 
-    /**
-     * Withdraw the funds
-     *
-     * Checks to see if goal or time limit has been reached, and if so, and the funding goal was reached,
-     * sends the entire amount to the beneficiary. If goal was not reached, each contributor can withdraw
-     * the amount they contributed.
-     */
     function safeWithdrawal() public afterDeadline {
         if (!fundingGoalReached) {
             uint amount = balanceOf[msg.sender];
@@ -112,7 +83,7 @@ contract Crowdsale {
             if (beneficiary.send(amountRaised)) {
                 emit FundTransfer(beneficiary, amountRaised, false);
             } else {
-                //If we fail to send the funds to beneficiary, unlock funders balance
+                
                 fundingGoalReached = false;
             }
         }
