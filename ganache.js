0c9d866f62da65be29bc8f5e33c0b5d3becdab8a
Konstantin Ustyuzhanin <ustyuzhanin.ky@gmail.com>
2019-01-14 22:26:23 +0500

    ganache.js has been deleted for purpouselessness

--- ganache.js
+++
@@ -1,9 +0,0 @@
-var ganache = require("ganache-cli");
-var server = ganache.server();
-var port = 8545
-server.listen(port, function(err, blockchain) {
-    console.log(blockchain)
-    if(err){
-        console.log(err)
-    }
-});
